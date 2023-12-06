7d2ae1dd9a2c94caad39f0ed64809b6c3c162fce
romw314 <106016361+romw314@users.noreply.github.com>
2023-12-06 14:35:06 +0100

    Update add-new-theme.cjs

--- scripts/add-new-theme.cjs
+++ scripts/add-new-theme.cjs
@@ -1,3 +1,19 @@
 const themeRaw = JSON.parse(process.argv[2]);
 console.log('theme raw:', themeRaw);
+const themeObj = {
+	fullName: themeRaw.fullName,
+	availability: {
+		monthSeason: [...themeRaw.availability]
+	},
+	bodyStyle: {
+		backgroundColor: themeRaw.bgcolor,
+		color: themeRaw.color
+	},
+	images: themeRaw.custimg ? undefined : '%-default-@.png',
+	squares: {
+		lightColor: [themeRaw.lscolor, themeRaw['lscolor-selected'], themeRaw['lscolor-canmove']],
+		darkColor: [themeRaw.dscolor, themeRaw['dscolor-selected'], themeRaw['dscolor-canmove']]
+	}
+};
+console.log('theme obj:', themeObj);
 process.exit(1);
